4c59d8c86a435e2b31b16c095cd5a9bc925dfdda
Harry Cutts <eternal.linux@gmail.com>
2012-04-23 12:43:55 +0100

    Neatened Denis' example code, and moved it into vision.cpp.
    Also began integrating the example code, and added initVision and
    disposeVision methods to vision.h.

--- include/vision.h
+++ include/vision.h
@@ -5,9 +5,16 @@ struct BallInfo_s;
 /** Contains data on the position of the ball relative to the robot. */
 typedef BallInfo_s BallInfo;
 
-/** @return A BallInfo struct containing information on the current position of the ball relative
- * to the webcam. */
-BallInfo see();
+/** Starts the vision system. Must be called before the vision system is used. */
+void initVision();
+
+/** Releases resources in use by the vision system. Should be called when the vision system 
+ * is no longer required. */
+void disposeVision();
+
+/** @return A pointer to a BallInfo struct containing information on the current position of the ball 
+ * relative to the webcam, or NULL if the ball cannot be found. */
+BallInfo* see();
 
 /** @return The horizontal angle between the ball and the normal from the plane of the webcam. */
 double getXRadians(BallInfo i);
---
+++ src/vision.cpp
@@ -0,0 +1,86 @@
+#include "../include/vision.h"
+#include <opencv/cv.h>
+
+#define NULL 0
+
+struct BallInfo_s {
+	double xRads;
+	double yRads;
+	double dist;
+}
+
+static bool visionInitialised = false;
+static CvCapture* camera;
+
+void initVision() {
+	camera = cvCreateCameraCapture(0);
+	assert(camera != NULL);
+	visionInitialised = true;
+}
+
+void disposeVision() {
+	cvReleaseCapture(&camera);
+	camera = NULL;
+	visionInitialised = true;
+}
+
+#define RED_MIN		128
+#define RED_MUL		4
+#define BLUEGREEN_MUL	3
+#define AREA_MIN	4000
+
+BallInfo* see() {
+	assert(visionInitialised);
+
+	IplImage* image=cvQueryFrame(camera);
+	unsigned char* pixelData = (unsigned char *)(image->imageData);
+		// pointer to iterate through image->imageData
+	int w = image->width;
+	int h = image->height;
+	int c = image->nChannels;	// used to iterate through pixels in pixelData
+
+	// Find the "Centre of mass" of the red pixels
+	int area = 0;
+	int xMoment = 0, yMoment = 0;
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; i < w; x++) {
+			// TODO: Calculate diameter
+			unsigned char* blue  = pixelData;
+			unsigned char* green = pixelData + 1;
+			unsigned char* red   = pixelData + 2;
+			if (((RED_MUL * (int)*red) > (BLUEGREEN_MUL * ((int)*blue + (int)* green))) 
+					& (*red > RED_MIN)) {
+				*red = 255;
+				area = area +1;
+				xMoment = xMoment + x;
+				yMoment = yMoment + y; 
+			} else {
+				*red = 0;
+			}
+			*blue = 0;
+			*green = 0;
+			pixelData = pixelData + c 
+		}
+	}
+	if (area > AREA_MIN) {
+		// ball found
+		int xPos = xMoment/area;
+		int yPos = yMoment/area;
+		// Draw a line on the image showing the x coordinate of the ball
+		/*unsigned char* blue_pixel =  (unsigned char *)(image->imageData) + c*xpos;
+		for (int y = 0; y < h; y++) {
+			* blue_pixel = 255;
+			blue_pixel = blue_pixel + c*w;
+		}*/
+		printf("Ball at (%d, %d)", xPos, yPos);
+		BallInfo* bi = (BallInfo)malloc(sizeof(BallInfo));
+		// TODO: define static functions
+		bi->xRads = pixelsToRads(xRads);
+		bi->yRads = pixelsToRads(yRads);
+		bi->dist = calculateDistance(diameter);
+		return bi;
+	} else {
+		return NULL;
+	}
+	cvwidget->putImage(image);
+}

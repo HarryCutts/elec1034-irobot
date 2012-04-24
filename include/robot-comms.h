c4e871706376742ec66ff1a812d11150da0679c6
an1g11 <an1g11@whale.(none)>
2012-04-24 12:17:02 +0100

    Modified robot-comms.h and added robot-comms.cpp

--- include/robot-comms.h
+++ include/robot-comms.h
@@ -3,9 +3,12 @@
 /** Sets the motor speeds to the given values. 
  * @param left	The speed of the left motor, in mm/s. 
  * @param right	The speed of the right motor, in mm/s. */
-void setMotorSpeeds(int left, int right);
+void setMotorSpeeds(s16 right, s16 left);
 
 /** Instructs the robot to follow a course around a circle.
  * @param velocity	The speed of the robot, in mm/s. 
  * @param radius	The radius of the circle in mms. */
-void setRobotCourse(int velocity, int radius);
+void setRobotCourse(s16 velocity, s16 radius);
+
+void initRobotComms();
+

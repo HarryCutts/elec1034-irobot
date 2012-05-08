3b9aee26ff98024f199e866cb53db268f981e63b
Simeon Jenkins <simwareonline@gmail.com>
2012-05-08 12:40:42 +0100

    Added in a returning pipe & modified to allow reading sensor data from robot

--- include/robot-comms.h
+++ include/robot-comms.h
@@ -14,4 +14,12 @@ void setRobotCourse(s16 velocity, s16 radius);
 
 void initRobotComms(void);
 
+//call this to request bump and wheel drop data from the robot. getSensorData MUST be the next motor function called
+void setSensorData(void);
+/*retrieve the requested sensor data from the robot. setSensorData must have been called first, with no other motor
+functions in between. This function is blocking but will take less time the longer ago that setSensorData was called -
+therefore, I suggest doing any processing in between these functions.
+Data is a byte, with bits 4:0 containing caster wheeldrop, left wheeldrop, right wheeldrop, bump left, bump right, respectively*/
+u8 getSensorData(void);
+
 void disposeRobotComms(void);
--- src/robot-comms.cpp
+++ src/robot-comms.cpp
@@ -13,11 +13,15 @@
 #include <assert.h>
 
 #define DEVICE "/dev/ttyUSB0"
-#define DRIVE (s16)137
-#define DRIVE_DIRECT (s16)145
-#define START_OI (s16)128
-#define SAFE_MODE (s16)131
-#define DELAY 10
+#define DRIVE (u8)137
+#define DRIVE_DIRECT (u8)145
+#define GET_SENSOR (u8)145
+#define SENSOR_BUMP_DROP (u8)7
+#define START_OI (u8)128
+#define SAFE_MODE (u8)131
+#define FULL_MODE (u8)132
+#define ROBOT_START_MODE SAFE_MODE
+#define DELAY 10 //ensures that bytes are not sent too frequently. Specifically, reads from robot sensors must not occur more often than every 15ms (but this requires 2 bytes sent, i.e. 2*10ms > 15ms, so delay=10 is okay)
 
 static void delay(s32);
 static void initSerialPort(void);
@@ -49,7 +53,8 @@ s32 main(void){
  * File descriptors.
  */
 static s32 ser;
-static s32 pipefd[2];
+static s32 pipeToRobotfd[2];
+static s32 pipeFromRobotfd[2];
 
 void sigproc(int signal) {
 	printf("Exit requested. Stopping.\n");
@@ -69,19 +74,25 @@ void initRobotComms(void){
 	signal(SIGINT, &sigproc);
 
 	// Pipe successful?
-	assert(pipe(pipefd) == 0);
+	assert(pipe(pipeToRobotfd) == 0);
+	assert(pipe(pipeFromRobotfd) == 0);
 
 	assert((cpid = fork()) != -1);
 
 	if (cpid == 0) {
-		close(pipefd[1]); // Close write end.
+		//we are the child
+		close(pipeToRobotfd[1]); // Close write end of pipe we read from.
+		close(pipeFromRobotfd[0]); // Close read end of pipe we write to.
 		serialRun();
 		exit(EXIT_SUCCESS);
 	} else {
-		close(pipefd[0]); // Close read end.
-		s16 resetRobot =START_OI,robotMode=SAFE_MODE;
-		write(pipefd[1],&resetRobot,1);
-		write(pipefd[1],&robotMode,1);
+		//we are the parent
+		close(pipeToRobotfd[0]); // Close read end of pipe we write to.
+		close(pipeFromRobotfd[1]); // Close write end of pipe we read from.
+		//put the robot into the right mode
+		s16 resetRobot =START_OI,robotMode=ROBOT_START_MODE;
+		write(pipeToRobotfd[1],&resetRobot,1);
+		write(pipeToRobotfd[1],&robotMode,1);
 	}
 }
 //Sets motor speeds to given values
@@ -92,6 +103,20 @@ void setMotorSpeeds(s16 right, s16 left){
 //Instructs the robot to follow a course around a circle
 void setRobotCourse(s16 velocity, s16 radius){
 	sendMotorCommand(DRIVE,velocity,radius);
+}
+
+//requests a "bump and wheel drops" packet from the robot
+void setSensorData(void){
+	u8 command = GET_SENSOR;
+	u8 packetNo = SENSOR_BUMP_DROP;
+	write(pipeToRobotfd[1],&command,1);
+	write(pipeToRobotfd[1],&packetNo,1);
+}
+//retrieves the sensor packet from the child; blocking
+u8 getSensorData(void){
+	u8 sensorData;
+	assert(read(pipeFromRobotfd[0],&sensorData,1)>0);
+	return sensorData;
 }
 
 /**
@@ -107,15 +132,15 @@ static void sendMotorCommand(u8 command, s16 param1, s16 param2){
 	param1High=(u8)(param1 >> 8);
 	param2High=(u8)(param2 >> 8);
 
-	write(pipefd[1],&command,1);
-	write(pipefd[1],&param1High,1);
-	write(pipefd[1],&param1Low,1);
-	write(pipefd[1],&param2High,1);
-	write(pipefd[1],&param2Low,1);
+	write(pipeToRobotfd[1],&command,1);
+	write(pipeToRobotfd[1],&param1High,1);
+	write(pipeToRobotfd[1],&param1Low,1);
+	write(pipeToRobotfd[1],&param2High,1);
+	write(pipeToRobotfd[1],&param2Low,1);
 }
 
 void disposeRobotComms(void){
-	close(pipefd[1]);
+	close(pipeToRobotfd[1]);
 	wait();
 }
 
@@ -143,16 +168,42 @@ static void initSerialPort(void){
 	cfmakeraw(&termsettings);
 	assert (tcsetattr(ser, TCSANOW, &termsettings) != -1);
 }
-
+//sends a byte on the serial port
+static void sendByte(u8 byte) {
+	assert (write(ser, &byte, 1) == 1);
+	assert (tcdrain(ser) != -1);
+	delay(DELAY);
+}
 static void serialRun(void) {
+	//set up the serial port
 	initSerialPort();
-	u8 nextByte;
-	while(read(pipefd[0],&nextByte,1)>0){
-		assert (write(ser, &nextByte, 1) == 1);
-		assert (tcdrain(ser) != -1);
-		delay(DELAY);
+	//when this is set to zero, the pipe is closed	
+	u8 pipe_to_parent_open=1;
+	//byte from the parent to the robot
+	u8 byteToRobot;
+	//byte from the robot to the parent
+	u8 byteFromRobot;
+	//this is 0 normally, gets set to 1 with a GET_SENSOR and gets set to 2 with a subsequent SENSOR_BUMP_DROP; when it is 2, we know we have to read from the robot.
+	u8 readyToReadFromRobot = 0;
+	while ((read(pipeToRobotfd[0],&byteToRobot,1)>0)){
+		//read a byte from the pipe; it will return 0 at eof so we know we can quit
+		if (byteToRobot == GET_SENSOR) {
+			readyToReadFromRobot = 1;
+		}
+		if ((readyToReadFromRobot == 1)&&(byteToRobot == SENSOR_BUMP_DROP)) {
+			readyToReadFromRobot = 2;
+		}
+		sendbyte(byteToRobot);
+		//see if there is a byte from the robot
+		if (readyToReadFromRobot == 2) {
+			assert(read(ser,&byteFromRobot,1)>0);
+			write(pipeFromRobotfd[1],&byteFromRobot,1);
+			readyToReadFromRobot = 0;
+		}
 	}
-	close(pipefd[0]);
+
+
+	close(pipeToRobotfd[0]);
 	close(ser);
 	return;
 }
